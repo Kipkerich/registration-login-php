e4b050720033f8b34ea9bcfb85416eb55126e896
Kipkerich <jkikerich@kabarak.ac.ke>
2023-10-27 18:15:18 +0300

    First commit

---
+++ templates/db.php
@@ -0,0 +1,20 @@
+<?php
+    // Enter your host name, database username, password, and database name.
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "loginsystem";
+    try {
+        //code...
+         // If you have not set database password on localhost then set empty.
+    // $con = mysqli_connect($servername,$username,$password,$database);
+    $con = new mysqli($servername,$username,$password,$database);
+    echo "connected";
+    
+    
+    } catch (\Throwable $th) {
+        //throw $th;
+        echo "error" . $th;
+    }
+   
+?>
